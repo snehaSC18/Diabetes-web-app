abbeecb3e23c236380c03a490cca3986154c15d2
Sneha Chattoraj <144983117+snehaSC18@users.noreply.github.com>
2025-03-23 23:56:10 +0530

    Update diabetes web app.py

--- diabetes web app.py
+++ diabetes web app.py
@@ -6,11 +6,13 @@ Created on Sun Mar 23 17:04:31 2025
 """
 
 import os
+import numpy as np
 import pickle
 import streamlit as st
 from streamlit_option_menu import option_menu
 
 #new for deploying
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # Construct the correct file path
 model_path = os.path.join(BASE_DIR, "trained_model.sav")
@@ -22,25 +24,19 @@ with open(model_path, 'rb') as model_file:
 #loaded_model = pickle.load(open('trained_model.sav','rb'))
 #creating a function for prediction
 def diabetes_prediction(input_data):
-    #input_data = (4,110,92,0,0,37.6,0.191,30)
+    # Ensure input_data is a list or tuple
+    if not isinstance(input_data, (list, tuple)):
+        raise ValueError("Input data should be a list or tuple")
 
-    # changing the input_data to numpy array
     input_data_as_numpy_array = np.asarray(input_data)
 
-    # reshape the array as we are predicting for one instance
-    input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+    # Reshape for model prediction (if necessary)
+    input_data_reshaped = input_data_as_numpy_array.reshape(1, -1)
 
-    # standardize the input data
-    #std_data = scaler.transform(input_data_reshaped)
-    #print(std_data)
-
+    # Make prediction
     prediction = loaded_model.predict(input_data_reshaped)
-    print(prediction)
 
-    if (prediction[0] == 0):
-      return'The person is not diabetic'
-    else:
-      return'The person is diabetic'
+    return "Diabetic" if prediction[0] == 1 else "Not Diabetic"
       
       
       
@@ -57,6 +53,15 @@ def main():
     BMI = st.text_input('BMI value')
     DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value')
     Age = st.text_input('Age of the Person')
+    Pregnancies = float(Pregnancies)
+    Glucose = float(Glucose)
+    BloodPressure = float(BloodPressure)
+    SkinThickness = float(SkinThickness)
+    Insulin = float(Insulin)
+    BMI = float(BMI)
+    DiabetesPedigreeFunction = float(DiabetesPedigreeFunction)
+    Age = float(Age)
+
     
     
     # code for Prediction
@@ -66,9 +71,16 @@ def main():
     
     if st.button('Diabetes Test Result'):
         diagnosis = diabetes_prediction([Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age])
-        
+        #user_input = [float(x) for x in user_input]
+
+        #diab_prediction = diabetes_model.predict([user_input])
+
+        #if diab_prediction[0] == 1:
+           # diab_diagnosis = 'The person is diabetic'
+       # else:
+            #diab_diagnosis = 'The person is not diabetic'
         
     st.success(diagnosis)
     
-if __name__ == '__main__':
-    main()
+#if __name__ == '__main__':
+#    main()
